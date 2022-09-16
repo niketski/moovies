1e41fd97eb92edee881c925a2683ef108cec460f
niketski <lim.nicolebenedict3@gmail.com>
2022-09-16 09:26:31 +0800

    created static tv series page

--- src/pages/TvSeries/TvSeries.js
+++ src/pages/TvSeries/TvSeries.js
@@ -1,9 +1,17 @@
+import styles from './TvSeries.module.css';
 import InnerPage from "../../components/blocks/inner-page/InnerPage";
+import QuickSearch from "../../components/quick-search/QuickSearch";
+import FilterTabs from '../../components/ui/filter-tabs/FilterTabs';
+import CardList from '../../components/card-list/CardList';
+import Pagination from '../../components/pagination/Pagination';
 
 const TvSeries = () => {
     return (
         <InnerPage title="TV Series">
-            tv-content
+            <QuickSearch className={styles.tvSeriresQuickSearch}/>
+            <FilterTabs className={styles.tvSeriesTabs} align="center"/>
+            <CardList className={styles.tvSeriesCardList}/>
+            <Pagination className={styles.tvSeriesPagination}/>
         </InnerPage>
     );
 }
